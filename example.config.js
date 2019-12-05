116ee6dd2a9dd32af7e6b29919ceca361b2b64aa
Pavel Birukov <pavel@birukov.me>
2019-12-05 12:27:02 +0200

    Fix RegExp syntax in example config

--- example.config.js
+++ example.config.js
@@ -2,11 +2,13 @@
 module.exports = {
     suppress: [
         {
+            // Ignore codes `7006, 7017` in `/src/js/general` directory
             pathRegExp: '/src/js/general',
             codes: [7006, 7017],
         },
         {
-            pathRegExp: '/packages/foo/src/engine/.*.spec..*',
+            // Ignore codes `2322, 2339, 2341, 2445, 2531, 2540` in *.spec.* files located in `/packages/foo/src/engine/`
+            pathRegExp: '/packages/foo/src/engine/.*\\.spec\\..*',
             codes: [2322, 2339, 2341, 2445, 2531, 2540],
         },
     ],
